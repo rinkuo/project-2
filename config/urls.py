fb6f69afc82bf0dd66a189d763afe5562c5938ab
Muhammadali <muhammadalikadyrov06@gmail.com>
2024-11-30 19:45:01 +0500

    app ochildi va html yozildi

--- config/urls.py
+++ config/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from courses.views import courses_list
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', courses_list)
 ]
